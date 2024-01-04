f8d2b038d9150e6fcf56686e1c006a1349c802a6
Marcos Jerez <markete86@gmail.com>
2024-01-04 00:34:17 -0300

    4

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -18,13 +18,13 @@ suite('Unit Tests', function () {
     test('#isOk, #isNotOk', function () {
       assert.isNotOk(null, 'null is falsey');
       assert.isOk("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isOk(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isTrue(true, 'true is true');
+      assert.isNotTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
